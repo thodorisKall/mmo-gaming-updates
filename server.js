b2b49158525984712df01d6fd58b9d6b2c7eab82
thodorisKall <105176440+thodorisKall@users.noreply.github.com>
2023-05-22 21:36:39 +0300

    docs: update server.js to use env file

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.get("/video-games", (req, res) => {
     method: "GET",
     url: "https://mmo-games.p.rapidapi.com/latestnews",
     headers: {
-      "X-RapidAPI-Key": "3a81af6b6cmshc8eb6a158aa8ac4p15cb04jsna09b248bc462",
+      "X-RapidAPI-Key": process.env.RAPIDAPI_KEY,
       "X-RapidAPI-Host": "mmo-games.p.rapidapi.com",
     },
   }
