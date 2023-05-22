fa1d1a7ff33a59dc0f496da8132195645f869d00
Thodoris Kallioras <105176440+thodorisKall@users.noreply.github.com>
2023-05-23 00:39:26 +0300

    docs: change server.js

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 const port = process.env.PORT || 8000;
-const express = require("express")
-const axios = require("axios")
-const app = express()
-const cors = require("cors")
-app.use(cors())
-require("dotenv").config()
-
+const express = require("express");
+const axios = require("axios");
+const app = express();
+const cors = require("cors");
+const path = require("path");
+app.use(cors());
+require("dotenv").config();
 
 app.get("/", (req, res) => {
   res.send("success");
@@ -14,7 +14,7 @@ app.get("/", (req, res) => {
 // rapid API
 
 app.get("/video-games", (req, res) => {
-  const axios = require("axios")
+  const axios = require("axios");
 
   const options = {
     method: "GET",
@@ -23,17 +23,24 @@ app.get("/video-games", (req, res) => {
       "X-RapidAPI-Key": process.env.RAPIDAPI_KEY,
       "X-RapidAPI-Host": "mmo-games.p.rapidapi.com",
     },
-  }
+  };
 
   axios
     .request(options)
     .then((response) => {
-      res.json(response.data.slice(0, 14))
-      console.log(response.data)
+      res.json(response.data.slice(0, 14));
+      console.log(response.data);
     })
     .catch((error) => {
-      console.error(error.message)
-    })
-})
+      console.error(error.message);
+    });
+});
 
-app.listen(port, () => console.log("Server Listening at port " + port))
+app.use(express.static(path.join(__dirname, "client")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "index.html"));
+});
+
+app.listen(port, () => console.log("Server Listening at port " + port));
+
